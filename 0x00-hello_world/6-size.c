15ad4b4f60a59ea5a86aa25370d0bea9ac118f13
DavidEtoh <Davidetoh295@gmail.com>
2023-03-16 08:36:05 -0700

    size of byte in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-/*
- *main - Entry point
+/**
+ *main - print the size of variuos type of c
  *
- * Return: Always 0 (success)
+ * Return: 0
  */
 int main(void)
 {
@@ -11,5 +11,5 @@ int main(void)
 	printf("size of a long int: %d byte(s)\n", sizeof(long int))
 	printf("size of a long long int: %d byte(s)\n", sizeof(long long int))
 	printf("size of a float: %d byte(s)\n", sizeof(float));
-	return(0)
+	return (0);
 }
