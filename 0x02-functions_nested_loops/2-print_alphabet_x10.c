4ad5c393df525698c098ff45ae31732450a5bd70
DavidEtoh <Davidetoh295@gmail.com>
2023-03-21 03:21:35 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+/**
+ * main - print 10 time the alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+
+	char ch;
+	int i;
+
+	while (i < 10)
+	{
+		ch = 'a'
+		while (ch <= 'z')
+		{
+			_puchar(ch);
+			ch++
+		}
+		_putchar('\n');
+		i++
+		return (0);
+	}
+}
