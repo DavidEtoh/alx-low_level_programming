72e15de13334e5d28274027555d32b09bd4d2524
DavidEtoh <Davidetoh295@gmail.com>
2023-03-21 09:03:25 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,27 +2,26 @@
 
 /**
  * print_sign - print the sign of a number
- * @c: the int to check
- * Return: 1 and print + if n is greater than zero
- * 0 and print 0 if n is zero
- * -1 and print - if n is less than zero
+ * @n: The number to be checked
+ * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
+
 int print_sign(int n)
+
 {
 	if (n > 0)
 	{
-		_putchar('+');
-		return (1);
+	_putchar(43);
+	return (1);
 	}
-	else if (n == 0)
+	else if (n < 0)
 	{
-		_putchar('0');
-		return (0);
+	_putchar(45);
+	return (-1);
 	}
 	else
-		(n < 0)
 	{
-		_putchar('-');
-		return (-1);
+	_putchar(48);
+	return (0);
 	}
 }
