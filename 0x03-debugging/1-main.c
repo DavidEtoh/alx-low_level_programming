f6923f0e80e17b8114e681530cb6e98d65c167b0
DavidEtoh <Davidetoh295@gmail.com>
2023-03-24 14:51:35 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,6 @@ int main(void)
 {
 
 int i;
-
 printf("Infinite loop incoming :(\n");
 	i = 0;
 
@@ -21,6 +20,5 @@ printf("Infinite loop incoming :(\n");
 	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
